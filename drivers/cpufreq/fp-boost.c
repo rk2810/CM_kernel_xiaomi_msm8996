cefa820dd579a3530511cb9b6cddcc9f6fc41e96
MOVZX <movzx@yahoo.com>
2017-12-12 07:51:55 +0300

    cpufreq: fp-boost: Don't mess up with normal home key press
    For some reason, this driver keeps boosting even with normally
    pressing home key and thus make the battery like a hell. To prevent
    this, add a check to handle the input event when screen is on.
    
    Change-Id: I447fe134b092e6158c3400189432a2ba675c8f07
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- drivers/cpufreq/fp-boost.c
+++ drivers/cpufreq/fp-boost.c
@@ -114,6 +114,9 @@ static int do_cpu_boost(struct notifier_block *nb,
 	if (action != CPUFREQ_ADJUST)
 		return NOTIFY_OK;
 
+	if (is_display_on())
+		return NOTIFY_OK;
+
 	state = get_boost_state(b);
 
 	/*
